15557cbd7ba1939a983562a64e939d754cbe8b84
Rohit Bhadani <bhadanirohit1@gmail.com>
2022-06-22 10:34:56 +0530

    v1 completed

--- src/pages/Education.js
+++ src/pages/Education.js
@@ -3,7 +3,33 @@ import React from "react";
 function Education() {
     return (
         <React.Fragment>
-            Education
+            <div className="min-h-screen">
+                <div className="container py-5">
+                    <h1 className="text-center mb-5">Education</h1>
+                    <div className="row">
+                        <div className="col-md-6 offset-md-3">
+                            <div className="card mb-5">
+                                <div className="card-body">
+                                    <h5 className="card-title fw-bold">Master of Engineering <span className="h6">from Silver Oak University</span></h5>
+                                    <p className="mb-0">APR 2016 - APR 2019</p>
+                                    <small>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus earum vel excepturi autem mollitia, blanditiis voluptatum perspiciatis alias eos architecto in, adipisci voluptatem corrupti quidem enim accusamus itaque non tempore!</small>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-md-6 offset-md-3">
+                            <div className="card mb-3">
+                                <div className="card-body">
+                                    <h5 className="card-title fw-bold">Master of Engineering <span className="h6">from Silver Oak University</span></h5>
+                                    <p className="mb-0">APR 2016 - APR 2019</p>
+                                    <small>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus earum vel excepturi autem mollitia, blanditiis voluptatum perspiciatis alias eos architecto in, adipisci voluptatem corrupti quidem enim accusamus itaque non tempore!</small>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </React.Fragment>
     );
 }
--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -3,7 +3,33 @@ import React from "react";
 function Experience() {
     return (
         <React.Fragment>
-            Experience
+            <div className="min-h-screen">
+                <div className="container py-5">
+                    <h1 className="text-center mb-5">Experience</h1>
+                    <div className="row">
+                        <div className="col-md-6 offset-md-3">
+                            <div className="card mb-5">
+                                <div className="card-body">
+                                    <h5 className="card-title fw-bold">Full Stack Developer <span className="h6">at Coding Gujju</span></h5>
+                                    <p className="mb-0">APR 2016 - PRESENT</p>
+                                    <small>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus earum vel excepturi autem mollitia, blanditiis voluptatum perspiciatis alias eos architecto in, adipisci voluptatem corrupti quidem enim accusamus itaque non tempore!</small>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-md-6 offset-md-3">
+                            <div className="card mb-3">
+                                <div className="card-body">
+                                    <h5 className="card-title fw-bold">FrontEnd Developer <span className="h6">at Microsoft</span></h5>
+                                    <p className="mb-0">APR 2016 - APR 2019</p>
+                                    <small>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus earum vel excepturi autem mollitia, blanditiis voluptatum perspiciatis alias eos architecto in, adipisci voluptatem corrupti quidem enim accusamus itaque non tempore!</small>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </React.Fragment>
     );
 }
