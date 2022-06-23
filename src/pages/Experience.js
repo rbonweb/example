89ee276c66ef7cacaef541bb929fbd8960b019d5
Rohit Bhadani <bhadanirohit1@gmail.com>
2022-06-23 11:19:55 +0530

    dynamic part

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -1,33 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 function Experience() {
+
+    const [experience, setExperience] = React.useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:3001/")
+            .then(res => res.json())
+            .then(data => {
+                setExperience(data);
+            });
+    }, []);
+
     return (
         <React.Fragment>
             <div className="min-h-screen">
                 <div className="container py-5">
                     <h1 className="text-center mb-5">Experience</h1>
-                    <div className="row">
-                        <div className="col-md-6 offset-md-3">
-                            <div className="card mb-5">
-                                <div className="card-body">
-                                    <h5 className="card-title fw-bold">Full Stack Developer <span className="h6">at Coding Gujju</span></h5>
-                                    <p className="mb-0">APR 2016 - PRESENT</p>
-                                    <small>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus earum vel excepturi autem mollitia, blanditiis voluptatum perspiciatis alias eos architecto in, adipisci voluptatem corrupti quidem enim accusamus itaque non tempore!</small>
+
+
+                    {experience.map((item, index) => (
+                        <div className="row" key={index}>
+                            <div className="col-md-6 offset-md-3">
+                                <div className="card mb-5">
+                                    <div className="card-body">
+                                        <h5 className="card-title fw-bold">{item.title} <span className="h6">at {item.company}</span></h5>
+                                        <p className="mb-0">{item.date}</p>
+                                        <small>{item.description}</small>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-md-6 offset-md-3">
-                            <div className="card mb-3">
-                                <div className="card-body">
-                                    <h5 className="card-title fw-bold">FrontEnd Developer <span className="h6">at Microsoft</span></h5>
-                                    <p className="mb-0">APR 2013 - APR 2016</p>
-                                    <small>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus earum vel excepturi autem mollitia, blanditiis voluptatum perspiciatis alias eos architecto in, adipisci voluptatem corrupti quidem enim accusamus itaque non tempore!</small>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    ))}
+
                 </div>
             </div>
         </React.Fragment>
