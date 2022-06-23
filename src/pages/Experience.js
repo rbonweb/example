c6550c219f8abd837df0ad7b34450ebe646ae6b2
Rohit Bhadani <bhadanirohit1@gmail.com>
2022-06-23 11:20:45 +0530

    dynamic added

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -2,15 +2,37 @@ import React, { useEffect } from "react";
 
 function Experience() {
 
-    const [experience, setExperience] = React.useState([]);
+    // const [experience, setExperience] = React.useState([]);
 
-    useEffect(() => {
-        fetch("http://localhost:3001/")
-            .then(res => res.json())
-            .then(data => {
-                setExperience(data);
-            });
-    }, []);
+    // useEffect(() => {
+    //     fetch("http://localhost:3001/")
+    //         .then(res => res.json())
+    //         .then(data => {
+    //             setExperience(data);
+    //         });
+    // }, []);
+
+    let experience = [
+        {
+            title: "Software Engineer",
+            company: "Google",
+            date: "May 2020 - Present",
+            description: "I am currently working on a project to build a new version of the Google Chrome browser. I am working on the frontend of the project, and I am working on the backend of the project. I am also working on the frontend of the project, and I am working on the backend of the project. I am currently working on a project to build a new version of the Google Chrome browser. I am working on the frontend of the project, and I am working on the backend of the project. I am also working on the frontend of the project, and I am working on the backend of the project."
+        },
+        {
+            title: "Full Engineer",
+            company: "Microfost",
+            date: "May 2016 - December 2019",
+            description: "Lorem Ipsum"
+        },
+        {
+            title: "Full Engineer",
+            company: "Microfost",
+            date: "May 2016 - December 2019",
+            description: "Lorem Ipsum"
+        }
+    ]
+
 
     return (
         <React.Fragment>
